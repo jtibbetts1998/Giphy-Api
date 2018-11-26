b78b5a336eaa88ee34293b7a88b454da8eb35954
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-11-26 14:35:31 -0500

    gifs now appear when user clicks first button

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+$(document).ready(function () {
 
+// This is the first buttons functionality as well as the ajax that calls on the user search data    
+$(".button01").on("click", function(event) {
+    event.preventDefault();
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending? + whattheytypedgoeshere + "api_key=zz4jHlgv2m2yKDgr63yd7o0E94HWtPO1&limit=4"
+    var resultsData = $(this).attr("data");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + resultsData + "&api_key=zz4jHlgv2m2yKDgr63yd7o0E94HWtPO1&limit=4";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
+// Looping the results and allowing the gifs to show on the page 
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImage);
+            $("#gifGoesHere").prepend(gifDiv);
+        }
+    })
 })
+    
+})
+
+
